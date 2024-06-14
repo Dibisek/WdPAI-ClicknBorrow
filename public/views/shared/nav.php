d79d7084eeff5b905528e2b5f6bfbf1d4de95a1d
Dibisek <114791693+Dibisek@users.noreply.github.com>
2024-06-14 17:53:35 +0200

    chore: Change icons and add titles in nav section

--- public/views/shared/nav.php
+++ public/views/shared/nav.php
@@ -4,27 +4,40 @@
     </a>
     <ul>
         <li>
-            <a href="/homepage" class="bar-btn">
+            <a href="/homepage" class="bar-btn" title="Homepage">
                 <i class="fa-solid fa-house"></i>
             </a>
         </li>
         <li>
-            <a href="/search" class="bar-btn">
+            <a href="/search" class="bar-btn" title="Search books">
                 <i class="fa-solid fa-magnifying-glass"></i>
             </a>
         </li>
+        <?php if (isAdmin()): ?>
+            <li>
+                <a href="/addBook" class="bar-btn" title="Add book">
+                    <i class="fa-solid fa-book-medical"></i>
+                </a>
+            </li>
+            <li>
+                <a href="/reservationsAdmin" class="bar-btn" title="Show pending reservations">
+                    <i class="fa-regular fa-address-card"></i>
+                </a>
+            </li>
+        <?php else: ?>
+            <li>
+                <a href="/bookmarks" class="bar-btn" title="Bookmarks">
+                    <i class="fa-solid fa-book-bookmark"></i>
+                </a>
+            </li>
+            <li>
+                <a href="/reservationsHistory" class="bar-btn" title="Reservations history">
+                    <i class="fa-regular fa-clock"></i>
+                </a>
+            </li>
+        <?php endif; ?>
         <li>
-            <a href="/bookmarks" class="bar-btn">
-                <i class="fa-solid fa-book-bookmark"></i>
-            </a>
-        </li>
-        <li>
-            <a href="/account" class="bar-btn">
-                <i class="fa-solid fa-clipboard-user"></i>
-            </a>
-        </li>
-        <li>
-            <a href="/logout" class="bar-btn">
+            <a href="/logout" class="bar-btn" title="Logout">
                 <i class="fa-solid fa-door-open"></i>
             </a>
         </li>
